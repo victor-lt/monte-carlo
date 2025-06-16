e1bd23b09c193505bc7491e871b0fd6d43ec307e
VLT <vlajouxthiebault@gmail.com>
2025-06-16 17:31:41 +0200

    starting main file and interface system

---
+++ main.py
@@ -0,0 +1,4 @@
+def interface(agents: list, rules: list, rounds: int, bankroll: int):
+    '''The interface plays the role of the dealer. He will,
+    against any 'agent' play a number of round following rules
+    and agent strategy'''
